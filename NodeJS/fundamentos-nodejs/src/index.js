b07d023713df6392062097ce80fd25ec4b242247
lCherS <lucascherez.cl@gmail.com>
2021-10-25 15:51:05 -0300

    C1Mod2Aula2: Nodemon adicionado

--- NodeJS/fundamentos-nodejs/src/index.js
+++ NodeJS/fundamentos-nodejs/src/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * GET - busca
+ * POST - insere
+ * PUT - altera
+ * PATCH - altera informação especifica
+ * DELETE - deleta
+ */
+
 app.get("/", (req, res) => {
   return res.json({
     name: "hello"
