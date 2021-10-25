62fc46f991649ef5c684b1935b4264a356fdc5c1
lCherS <lucascherez.cl@gmail.com>
2021-10-25 16:34:12 -0300

    C1Mod2Aula3: Adicionando metodos HTTP

--- NodeJS/fundamentos-nodejs/src/index.js
+++ NodeJS/fundamentos-nodejs/src/index.js
@@ -10,10 +10,32 @@ const app = express();
  * DELETE - deleta
  */
 
-app.get("/", (req, res) => {
-  return res.json({
-    name: "hello"
-  })
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curos 3"])
+})
+
+app.post('/courses', (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curos 3", "Curso 4"])
+})
+
+app.put('/courses/:id/:nome', (req, res) => {
+  const id = req.params.id;
+  const nome = req.params.nome;
+  let cursos = ["Curso 1", "Curso 2", "Curos 3", "Curso 4"];
+  cursos[id] = nome;
+
+  return res.json(cursos);
+})
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 5", "Curso 2", "Curos 3", "Curso 4"])
+})
+
+app.delete("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  let cursos = ["Curso 1", "Curso 2", "Curos 3", "Curso 4"];
+
+  return res.json(cursos);
 })
 
 app.listen(3333);
