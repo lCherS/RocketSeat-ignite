605d7c884c7f15d49c74d1353d01f388e1133c99
lCherS <lucascherez.cl@gmail.com>
2021-10-26 16:50:09 -0300

    FINAPI: Pegando extrato

--- NodeJS/FINAPI/src/index.js
+++ NodeJS/FINAPI/src/index.js
@@ -1,5 +1,5 @@
-const express  = require('express');;
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -15,29 +15,34 @@ app.use(express.json());
 const customers = [];
 
 app.post("/account", (req, res) => {
-  const {cpf, name} = req.body; 
+  const { cpf, name } = req.body;
 
-  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-  if(customerAlreadyExists){
+  if (customerAlreadyExists) {
     return res.status(400).json({
-      error: "Customer already exists!"
-    })
-  }
-
-  customers.push({
-      cpf,
-      name,
-      id: uuidv4(),
-      statement: []
+      error: "Customer already exists!",
     });
-
-    console.log(customers);
-
-    return res.status(201).send();
+  }
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return res.status(201).send();
 });
 
-console.log('hello')
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return res.json(customer.statement);
+});
+
+console.log("hello");
+
 app.listen(3333);
-
-
