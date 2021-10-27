5aba90c9ccb2580b4805f5e274795d8f35f22dca
lCherS <lucascherez.cl@gmail.com>
2021-10-27 11:33:09 -0300

    FINAPI: Adicionando deposito

--- NodeJS/FINAPI/src/index.js
+++ NodeJS/FINAPI/src/index.js
@@ -52,7 +52,25 @@ app.post("/account", (req, res) => {
 app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
   const { customer } = req;
 
-  return res.json(customer.statement);
+  return res.json(customer);
 });
+
+app.post("/deposit",verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    create_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send("sucesso");
+
+})
 
 app.listen(3333);
