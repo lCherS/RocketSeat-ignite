74724272b14158ec95eadbd61d20aca0a3691b25
lCherS <lucascherez.cl@gmail.com>
2021-11-01 15:24:53 -0300

    Adicionando uuid

--- NodeJS/ChapterII/ModII/src/routes/categories.routes.ts
+++ NodeJS/ChapterII/ModII/src/routes/categories.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-  const NewCategory = { name, description };
+  const NewCategory = {
+    id: uuidV4(),
+    name,
+    description,
+  };
   categories.push(NewCategory);
 
   return res.status(201).json(NewCategory);
