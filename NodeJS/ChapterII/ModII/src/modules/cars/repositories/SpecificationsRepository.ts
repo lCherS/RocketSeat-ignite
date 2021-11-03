8954f0c3659df13d8279335074e230c12e3a4bf1
lCherS <lucascherez.cl@gmail.com>
2021-11-03 15:17:14 -0300

    inserindo uma especificacao

---
+++ NodeJS/ChapterII/ModII/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,31 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+  findByName(name: string): Specification {
+    const spec = this.specifications.find((specific) => specific.name === name);
+    return spec;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO) {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationsRepository };
--- NodeJS/ChapterII/ModII/src/modules/cars/services/CreateSpecificationService.ts
+++ NodeJS/ChapterII/ModII/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,18 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("oi");
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationExists = this.specificationRepository.findByName(name);
+
+    if (specificationExists) throw new Error("Specifications already exists!");
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
