407d0420d4aa596c26cb91ced046cb75e4b5e461
lCherS <lucascherez.cl@gmail.com>
2021-11-04 14:46:18 -0300

    conceito de stream e leitura de dados

--- NodeJS/ChapterII/ModII/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ NodeJS/ChapterII/ModII/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,17 @@
-// class ImportCategory
+import csvParse from "csv-parse";
+import fs from "fs";
 
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
