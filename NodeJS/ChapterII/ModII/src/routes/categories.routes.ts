59e395ee292a9762802e286543aedcd85c545d6d
lCherS <lucascherez.cl@gmail.com>
2021-11-04 11:53:21 -0300

    recebendo files

--- NodeJS/ChapterII/ModII/src/routes/categories.routes.ts
+++ NodeJS/ChapterII/ModII/src/routes/categories.routes.ts
@@ -1,16 +1,26 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
-
+const upload = multer({
+  dest: "./tmp",
+});
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+
+  return res.send();
 });
 
 export { categoriesRoutes };
